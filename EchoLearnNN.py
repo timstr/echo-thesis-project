Fixed temporal moment computation

EchoLearnNN.py
@@ -175,8 +175,9 @@ class EchoLearnNN(nn.Module):
 
         if self._input_config.summary_statistics:
             ls = torch.linspace(0.0, 1.0, N).unsqueeze(0).unsqueeze(0).to(wx.device)
+            wx = torch.sigmoid(wx)
 
-            mom1 = torch.sum(wx * ls, dim=2)
+            mom1 = torch.sum(wx * ls, dim=2) / torch.sum(wx, dim=2)
             assert mom1.shape == (B, F)
 
             mom2 = torch.sum((wx * (ls - mom1.unsqueeze(-1)))**2, dim=2)

echolearn.py
@@ -164,7 +164,7 @@ def main():
         fname = f"{args.experiment}_{timestamp}_{label}.dat"
         return os.path.join(model_path, fname)
 
-    optimizer = torch.optim.Adam(network.parameters(), lr=0.005)
+    optimizer = torch.optim.Adam(network.parameters(), lr=0.001, weight_decay=0.001)
 
     timestamp = datetime.datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
 
@@ -207,7 +207,6 @@ def main():
                 batch_gpu = batch_cpu.to(the_device)
 
                 pred_gpu = network(batch_gpu)
-                pred_cpu = pred_gpu.to('cpu')
                 
                 loss, loss_terms = loss_function(batch_gpu, pred_gpu)
                 optimizer.zero_grad()