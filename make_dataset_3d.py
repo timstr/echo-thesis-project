71b875f107ce5fecea942f82668643d46abac90e
timstr <straubinger.tim@gmail.com>
2021-08-04 17:46:12 -0700

    Added inner and outer random obstacles to dataset creation

--- make_dataset_3d.py
+++ make_dataset_3d.py
@@ -2,6 +2,7 @@ import fix_dead_command_line
 
 import h5py
 import numpy as np
+import torch
 import os
 from argparse import ArgumentParser
 
@@ -10,11 +11,68 @@ from dataset3d import WaveDataset3d
 from current_simulation_description import (
     make_random_obstacles,
     make_simulation_description,
+    minimum_x_units,
+    Nx,
+    Ny,
+    Nz,
 )
+from utils import assert_eq
+
+
+def middle_is_empty(obstacles):
+    assert isinstance(obstacles, torch.Tensor)
+    assert_eq(obstacles.shape, (Nx, Ny, Nz))
+    assert_eq(obstacles.dtype, torch.bool)
+    xlo = minimum_x_units + ((Nx - minimum_x_units) // 4)
+    xhi = minimum_x_units + ((Nx - minimum_x_units) * 3 // 4)
+    ylo = Ny // 4
+    yhi = Ny * 3 // 4
+    zlo = Nz // 4
+    zhi = Nz * 3 // 4
+    return torch.all(obstacles[xlo:xhi, ylo:yhi, zlo:zhi] == 0)
+
+
+def outside_is_empty(obstacles):
+    assert isinstance(obstacles, torch.Tensor)
+    assert_eq(obstacles.shape, (Nx, Ny, Nz))
+    assert_eq(obstacles.dtype, torch.bool)
+    xlo = minimum_x_units + ((Nx - minimum_x_units) // 4)
+    xhi = minimum_x_units + ((Nx - minimum_x_units) * 3 // 4)
+    ylo = Ny // 4
+    yhi = Ny * 3 // 4
+    zlo = Nz // 4
+    zhi = Nz * 3 // 4
+    #  front
+    # ########
+    # ########
+    # ########
+    # ########
+    #
+    #  middle
+    # ######## <- middle top
+    # ##    ## <- middle left and right
+    # ######## <- middle bottom
+    #
+    #  back
+    # ########
+    # ########
+    # ########
+    # ########
+
+    return (
+        torch.all(obstacles[:xlo, :, :] == 0)  # front
+        and torch.all(obstacles[xhi:, :, :] == 0)  # back
+        and torch.all(obstacles[xlo:xhi, :ylo, :] == 0)  # middle bottom
+        and torch.all(obstacles[xlo:xhi, yhi:, :] == 0)  # middle top
+        and torch.all(obstacles[xlo:xhi, ylo:yhi, :zlo] == 0)  # middle left
+        and torch.all(obstacles[xlo:xhi, ylo:yhi, zhi:] == 0)  # middle right
+    )
 
 
 def main():
     mode_random = "random"
+    mode_random_outer = "random-outer"
+    mode_random_inner = "random-inner"
     mode_orbiting_sphere = "orbiting-sphere"
     mode_echo4ch = "echo4ch"
 
@@ -25,7 +83,13 @@ def main():
     parser.add_argument(
         "--mode",
         type=str,
-        choices=["random", "orbiting-circle", "echo4ch"],
+        choices=[
+            mode_random,
+            mode_random_inner,
+            mode_random_outer,
+            mode_orbiting_sphere,
+            mode_echo4ch,
+        ],
         dest="mode",
         required=True,
     )
@@ -47,15 +111,26 @@ def main():
             "Please set the DATASET_OUTPUT environment variable to point to the HDF5 dataset file that should be created"
         )
 
-    if mode == mode_random:
+    if mode in [mode_random, mode_random_inner, mode_random_outer]:
         if count is None:
             raise Exception(
                 "Please specify the --count argument when using random mode"
             )
 
+        def filter(obs):
+            if mode == mode_random_inner:
+                return outside_is_empty(obs)
+            elif mode == mode_random_outer:
+                return middle_is_empty(obs)
+            return True
+
         def obstacle_generator():
             for i in range(count):
-                yield make_random_obstacles(desc), "random obstacles"
+                while True:
+                    obs = make_random_obstacles(desc)
+                    if filter(obs):
+                        break
+                yield obs, mode
 
     elif mode == mode_orbiting_sphere:
         raise Exception("orbiting-sphere mode is not implemented")
