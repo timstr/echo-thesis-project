b1a4e54f0320b1ea8b76129bc57b8d769b7dd603
timstr <straubinger.tim@gmail.com>
2021-08-14 14:10:19 -0700

    Added tools for datasets

--- combine_datasets.py
+++ combine_datasets.py
@@ -6,23 +6,28 @@ from current_simulation_description import make_simulation_description
 from dataset3d import WaveDataset3d
 from utils import progress_bar
 
-dataset_name = "random"
 
-
-def main(dataset_base_name, num_parts):
-    assert isinstance(dataset_base_name, str)
+def main(output_path, part_base_path, num_parts, append):
+    assert isinstance(output_path, str)
+    assert isinstance(part_base_path, str)
     assert isinstance(num_parts, int) and num_parts > 0
+    assert isinstance(append, bool)
     desc = make_simulation_description()
-    new_path = f"/{dataset_base_name}.h5"
-    if os.path.exists(new_path):
+    if os.path.exists(output_path):
+        if not append:
+            print(
+                f"Error: attempted to create a new dataset at {output_path} but it already exists"
+            )
+            exit(-1)
+    elif append:
         print(
-            f"Error: attempted to create a new dataset at {dataset_base_name}.h5 but it already exists"
+            f"Error: attempted to append to a dataset at {output_path} but it doesn't exist"
         )
         exit(-1)
-    with WaveDataset3d(desc, new_path, write=True) as new_ds:
+    with WaveDataset3d(desc, output_path, write=True) as new_ds:
         for i_part in range(num_parts):
             print(f"Dataset part {i_part+1} of {num_parts}")
-            old_path = f"/{dataset_base_name}_{i_part+1}_of_{num_parts}.h5"
+            old_path = f"{part_base_path}_{i_part+1}_of_{num_parts}.h5"
             if not os.path.exists(old_path):
                 print(f"Error: tried to open the file {old_path} but it doesn't exist")
                 exit(-1)
@@ -38,13 +43,21 @@ def main(dataset_base_name, num_parts):
 
 
 def print_usage_and_exit():
-    print(f"Usage: {sys.argv[0]} dataset_base_name_without_suffix count")
+    print(
+        f"Usage: {sys.argv[0]} combined_output.h5 part_base_path_without_suffix count [--append]"
+    )
     exit(-1)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
+    if len(sys.argv) not in [4, 5]:
         print_usage_and_exit()
-    dataset_base_name = sys.argv[1]
-    num_parts = int(sys.argv[2])
-    main(dataset_base_name, num_parts)
+    output_path = sys.argv[1]
+    part_base_path = sys.argv[2]
+    num_parts = int(sys.argv[3])
+    append = False
+    if len(sys.argv) == 5:
+        if sys.argv[4] != "--append":
+            print_usage_and_exit()
+        append = True
+    main(output_path, part_base_path, num_parts, append)
